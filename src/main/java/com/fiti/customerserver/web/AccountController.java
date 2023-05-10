72cca6658640adaee9202279f5c0b0a89f6e2db4
SY2on <sjk8w7@gmail.com>
2023-05-10 22:07:54 +0900

    Feat: 회원가입시 이메일 중복 체크 기능 개발
    회원 가입시 이메일 중복 체크 기능 API 개발
    
    Resolves: #5

--- src/main/java/com/fiti/customerserver/web/AccountController.java
+++ src/main/java/com/fiti/customerserver/web/AccountController.java
@@ -4,12 +4,11 @@ import com.fiti.customerserver.response.BaseResponse;
 import com.fiti.customerserver.response.exception.BusinessException;
 import com.fiti.customerserver.service.AccountService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fiti.customerserver.web.AccountDto.*;
 
@@ -29,6 +28,17 @@ public class AccountController {
         accountService.join(join.getEmail(), join.getPassword(), join.getName());
         return new BaseResponse("ok");
     }
+    /**
+     * 이메일 중복 체크
+     */
+    @Operation(summary = "이메일 중복 체크", description = "고객 회원가입시 이메일 중복 체크")
+    @GetMapping("email-duplicate/{email}")
+    public BaseResponse<EmailDuplicateCheck> joinValidation(@Parameter(name = "email", description = "이메일",
+            in = ParameterIn.PATH) @PathVariable String email){
+        boolean emailDuplicated = accountService.emailDuplicateCheck(email);
+        return new BaseResponse(new EmailDuplicateCheck(emailDuplicated));
+    }
+
     /**
      * 로그인
      */
